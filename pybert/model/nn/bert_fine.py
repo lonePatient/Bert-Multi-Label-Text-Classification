46d82e08f41096b8d1299265beb58279a7af3a63
Desiree Vogt-Lee <desireevogtlee@gmail.com>
2019-08-09 11:54:03 +1000

    bert_fine update

--- pybert/model/nn/bert_fine.py
+++ pybert/model/nn/bert_fine.py
@@ -9,7 +9,7 @@ class BertFine(BertPreTrainedModel):
         self.bert = BertModel(bertConfig) # bert模型
         self.dropout = nn.Dropout(bertConfig.hidden_dropout_prob)
         self.classifier = nn.Linear(in_features=bertConfig.hidden_size, out_features=num_classes)
-        self.apply(self.init_bert_weights)
+        self.apply(self.init_weights)
         # 默认情况下，bert encoder模型所有的参数都是参与训练的，32的batch_size大概8.7G显存
         # 可以通过以下设置为将其设为不训练，只将classifier这一层进行反响传播，32的batch_size大概显存1.1G
         self.unfreeze_bert_encoder()
@@ -25,8 +25,8 @@ class BertFine(BertPreTrainedModel):
     def forward(self, input_ids, token_type_ids, attention_mask, label_ids=None, output_all_encoded_layers=False):
         _, pooled_output = self.bert(input_ids,
                                      token_type_ids,
-                                     attention_mask,
-                                     output_all_encoded_layers=output_all_encoded_layers)
+                                     attention_mask)
+                                     #output_all_encoded_layers=output_all_encoded_layers)
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         return logits
